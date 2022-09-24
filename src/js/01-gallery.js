3f89ced744eac401530ebb1c297620fd3bf8d51d
VeronikaTerletska <104487544+VeronikaTerletska@users.noreply.github.com>
2022-09-24 21:31:18 +0300

    change code

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,66 +1,33 @@
-// import SimpleLightbox from 'simplelightbox';
-// // Додатковий імпорт стилів
-// import 'simplelightbox/dist/simple-lightbox.min.css';
+import simpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
-// const picturesContainer = document.querySelector(".gallery");
-// const galleryMarkup = createGalleryItemsMarkup(galleryItems);
+const picturesContainer = document.querySelector('.gallery');
+const galleryMarkup = createGalleryItemsMarkup(galleryItems);
 
-// picturesContainer.insertAdjacentHTML("beforeend", galleryMarkup);
+picturesContainer.insertAdjacentHTML('beforeend', galleryMarkup);
 
-// picturesContainer.addEventListener("click", onPicturesContainerClick);
-
-// function createGalleryItemsMarkup(galleryItems) {
-//   return galleryItems
-//     .map(({ preview, original, description }) => {
-//       return `<div class="gallery__item">
-//   <a class="gallery__link" href="${original}">
-//     <img
-//       class="gallery__image"
-//       src="${preview}"
-//       data-source="${original}"
-//       alt="${description}"
-//     />
-//   </a>
-// </div>`;
-//     })
-//     .join("");
-// }
-// //
-// function onPicturesContainerClick(event) {
-//   event.preventDefault();
-
-//   const isGalleryImageEl = event.target.classList.contains("gallery__image");
-
-//   if (!isGalleryImageEl) {
-//     return;
-//   }
-//   const galleryOriginalUrl = event.target.dataset.source;
-
-//   const instance = basicLightbox.create(
-//     `
-//     <img src="${galleryOriginalUrl}" width="800" height="600">
-// `,
-//     {
-//       onShow: instance => {
-//         document.addEventListener("keydown", function (event) {
-//           if (event.key === "Escape") {
-//             return instance.close();
-//           }
-//         });
-//       },
-//       onClose: instance => {
-//         document.addEventListener("keydown", function (event) {
-//           if (event.key === "Escape") {
-//             return instance.close();
-//           }
-//         });
-//       },
-//     }
-//   );
-
-//   instance.show();
-// }
+function createGalleryItemsMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`;
+    })
+    .join('');
+}
+let gallery = new simpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionsDelay: 250,
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+import VimeoPlayer from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new VimeoPlayer(iframe);
+const LOCAL_STORAGE_KEY = 'videoplayer-current-time';
+
+player.on('timeupdate', throttle(timePlay, 10000));
+
+function timePlay({ seconds }) {
+  localStorage.setItem('LOCAL_STORAGE_KEY', seconds);
+}
+
+function initPage() {
+  let saveData = localStorage.getItem('LOCAL_STORAGE_KEY');
+  if (saveData) {
+    player.setCurrentTime(saveData);
+  }
+}
+initPage();
